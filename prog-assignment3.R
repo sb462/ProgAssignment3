3a677718fd30bcc49ed65b1690818b5516b317e7
sb462 <sandip.biswas@gmail.com>
2014-05-27 23:19:15 -0500

    -commit 05.27

--- prog-assignment3.R
+++ prog-assignment3.R
@@ -9,12 +9,13 @@ grep
 ?
 k<-unlist(strsplit("heart attack", " "))
 k[1]
-?grep
+?subset
 grep(paste(k[1],k[2],sep="."),y,ignore.case=TRUE)
 && grep("hospital.30.day",y,ignore.case=TRUE)
 paste(k, sep ="/",collapse =".")
+?grep
 ?paste
-
+?colNames
 ?tapply
 data_mortality_rate <- as.numeric(data_outcome[,11])
 ?hist
@@ -22,13 +23,29 @@ data_mortality_rate <- as.numeric(data_outcome[,11])
 hist(data_mortality_rate)
 ?pmatch
 g<-data_outcome$State=="IL"
+library(sqldf)
+?grep
+?min
+?sort
+z<- sort(c("c","a","b"))
 
 best <- function(state=character(),disease = character()){
-  outcome_by_state <- data_outcome[data_outcome$State==state,]
-  disease_name <- paste(unlist(strsplit(disease, " ")),collapse =".")
-  outcome_name <- paste("Hospital.30.Day.Death..Mortality..Rates.from",disease_name,sep=".")
-  outcome_by_state[,outcome_by_state$outcome_name]<- as.numeric(outcome_by_state[,outcome_by_state$outcome_name])
-  print(min(outcome_by_state$outcome_name))
+  outcome_by_state <- subset(data_outcome, State = "IL")
+  #summary(outcome_by_state)
+  disease_name <- paste(unlist(strsplit(disease, " ")),collapse =".*")
+  outcome_name <- paste("^Hospital.30.Day.Death..Mortality..Rates.from",disease_name,"$",sep=".*")
+  #print(outcome_name)
+  columnNames <- colnames(outcome_by_state)
+  ii<- grep(outcome_name,columnNames,ignore.case=TRUE)
+  outcome_by_state[,ii]<- as.numeric(outcome_by_state[,ii])
+  minm_rate <- min(outcome_by_state[,ii],na.rm=TRUE)
+  #print(minm_rate)
+  best_hospital_names <- sort(subset(outcome_by_state,outcome_by_state[,ii]==minm_rate)$Hospital.Name)
+  #hospital.name <- b
+  print(best_hospital_names[1])
+  
+  #outcome_by_state[,outcome_by_state$outcome_name]<- as.numeric(outcome_by_state[,outcome_by_state$outcome_name])
+  #print(min(outcome_by_state$outcome_name))
   #best_hospital_index <- which(outcome_by_state$outcome_name)= min(outcome_by_state[,outcome_by_state$outcome_name])
   #best_hospital_name <- outcome_by_state$Hospital.Name[best_hospital_index]
   #print(best_hospital_name)
